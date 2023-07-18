06723a66dad08662f669bcf0f8121cb115a3fe7a
Andres <cesarandrestorresbernal604@gmail.com>
2023-07-17 23:13:23 -0500

    vistas terminadas 100% faltan javascript

---
+++ Javascript/login_Admin.js
@@ -0,0 +1,26 @@
+let form = document.getElementById('form_Parking');
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log("enviando")
+    let form_data = new FormData(form)
+    let data= {
+        id_Admin: 1,
+        zone: form_data.get('Zone'),
+        quantity_space_moto: form_data.get('motos'),
+        quantity_space_car: form_data.get('heavy'),
+        quantity_space_heavy: form_data.get('autos'),
+        username: "",
+        email: "",
+        password: ""
+    }
+    fetch("http://localhost:4200/Parkings/View-Parkings/",{
+        method: 'POST',
+        type:'json',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    }).then((response)=>response.json())
+    .then((response)=> {
+        console.log(response)
+        alert("El parqueadero se a creado con exito en la zona: "+data.zone)
+    })
+});
--- Javascript/minute_Contract.js
+++ Javascript/minute_Contract.js
@@ -10,12 +10,46 @@ function fillSelectVehicle() {
         select.appendChild(optionElement);
     });
 }
+
 fillSelectVehicle();
+
 let form = document.getElementById('form_Contract_Minute');
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log("enviando")
-    let form_data = new FormData(form)
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    console.log("enviando");
+    let form_data = new FormData(form);
+    let data_Client = {
+        name: 'USER',
+        phone: '---',
+        identification: '---'
+    };
+    let data_Contract = {
+        id_Admin: 1,
+        type: 'Minuto',
+    };
+
+    await fetch("http://localhost:4200/Clients/Contract/", {
+        method: 'POST',
+        type: 'json',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data_Contract)
+    }).then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+            return response;
+        });
+
+    let new_User = await fetch("http://localhost:4200/Clients/Manage/", {
+        method: 'POST',
+        type: 'json',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data_Client)
+    }).then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+            return response;
+        });
+
     let data_Vehicle = {
         id_Parking: 1,
         color: '---',
@@ -23,30 +57,32 @@ form.addEventListener('submit', (e) => {
         brand: '---',
         model: '---',
         type: form_data.get('selectVechiclesType')
-    }
-    let data_Client = {
-        name: 'USER',
-        phone: '---',
-        identification: '---'
-    }
-    fetch("http://localhost:4200/Clients/Manage/", {
-        method: 'POST',
-        type: 'json',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data_Client)
-    }).then((response) => response.json())
-        .then((response) => {
-            console.log(response)
-            //alert("El parqueadero se a creado con exito en la zona: "+data.brand)
-        })
-    fetch("http://localhost:4200/Vehicles/View-Vehicles/", {
+    };
+
+    let new_Vehicle = await fetch("http://localhost:4200/Vehicles/View-Vehicles/", {
         method: 'POST',
         type: 'json',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data_Vehicle)
     }).then((response) => response.json())
         .then((response) => {
-            console.log(response)
-            //alert("El parqueadero se a creado con exito en la zona: "+data.brand)
-        })
+            console.log(response);
+            return response;
+        });
+
+    let data_Receipt = {
+        id_Client: new_User.id,
+        id_Vehicle: new_Vehicle.id,
+        date_entry: new Date()
+    };
+    console.log(data_Receipt);
+    fetch("http://localhost:4200/Clients/Receipt/", {
+        method: 'POST',
+        type: 'json',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data_Receipt)
+    }).then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+        });
 });
